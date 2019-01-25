79c7e61850b343115171a08881a21795cfa49030
phptheme <dev@getphptheme.com>
2019-01-25 14:17:15 +0300

    n/a

--- Views/footer-button.php
+++ Views/footer-button.php
@@ -2,7 +2,7 @@
 <?= PHPTheme::beginTag('a', $options);?>
   <span class="fa-stack fa-lg">
     <i class="fas fa-circle fa-stack-2x"></i>
-    <i class="fab fa-<?= $icon;?> fa-stack-1x fa-inverse"></i>
+    <i class="<?= $icon;?> fa-stack-1x fa-inverse"></i>
   </span>
 <?= PHPTheme::endTag('a');?>
 </li>
