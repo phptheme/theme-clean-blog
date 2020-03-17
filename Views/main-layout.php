e4240c953d7b51f2f20800bc08cf372a3186099c
phptheme <dev@getphptheme.com>
2020-03-17 21:19:24 +0300

    n/a

--- Views/main-layout.php
+++ Views/main-layout.php
@@ -33,7 +33,7 @@
 <div class="container">
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-            \
+            
             <?php if($title):?>
 
                 <h1><?= $title;?></h1>
